e33c52a6a470790ab93cec300e7871932216906b
Eve-Sama <948832626@qq.com>
2022-09-24 22:31:07 +0800

    feat(dataCard): set 0 value as gray

--- src/pages/common/data-card/data-card.tsx
+++ src/pages/common/data-card/data-card.tsx
@@ -1,17 +1,34 @@
 import styles from './style.module.scss';
+interface DataCardProps {
+  title: string;
+  changeValue: number;
+  totalValue: number;
+}
 
-export function DataCard(props: { title: string; changeValue: number; totalValue: number }) {
+export function DataCard(props: DataCardProps) {
   const { title, changeValue = 0, totalValue = 0 } = props;
-  const reg = /(?=(\B\d{3})+$)/g;
+
   const getFormaValue = (value: number): string => {
-    return value.toString().replace(reg, ',');
+    return value.toLocaleString();
+  };
+
+  const changeValueComponent: () => string = () => {
+    let className = '';
+    if (changeValue === 0) {
+      className = styles['change-value-zero'];
+    } else if (changeValue > 0) {
+      className = styles['change-value-positive'];
+    } else {
+      className = styles['change-value-nagative'];
+    }
+    return className;
   };
 
   return (
     <div className={styles['container']}>
       <div className={styles['header']}>
         <div className={styles['title']}>{title}</div>
-        <div className={styles['change-value']}>{changeValue}</div>
+        <div className={changeValueComponent()}>{changeValue}</div>
       </div>
       <div className={styles['total-value']}>{getFormaValue(totalValue)}</div>
     </div>
