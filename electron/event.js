fb378fe56d6207e6d7eb8b48a54eafa1070fdef6
Eve-Sama <948832626@qq.com>
2022-11-03 12:45:07 +0800

    fix: fix run error when set cookie

--- electron/event.js
+++ electron/event.js
@@ -6,20 +6,22 @@ const { windowMap } = require('./window');
 const store = new Store();
 
 function setCookie() {
-  const fn = (type, details) => {
-    if (details.url.indexOf(type) !== -1) {
-      const { cookie } = (store.get(`${type}-data`) || {}).config;
-      details.requestHeaders['Referer'] = null;
-      details.requestHeaders['cookie'] = cookie;
+  const updateCookie = (key, details) => {
+    if (details.url.indexOf(key) !== -1) {
+      const data = store.get(`${key}-data`);
+      if (data) {
+        details.requestHeaders['Referer'] = null;
+        details.requestHeaders['cookie'] = data.config.cookie;
+      }
     }
   };
   const filter = {
     urls: [],
   };
   session.defaultSession.webRequest.onBeforeSendHeaders(filter, (details, callback) => {
-    fn('juejin', details);
-    fn('bilibili', details);
-    fn('zhihu', details);
+    updateCookie('juejin', details);
+    updateCookie('bilibili', details);
+    updateCookie('zhihu', details);
     callback({ cancel: false, requestHeaders: details.requestHeaders });
   });
 }
